302f015c3cfe1cf7cfc1b8cc8eed484fb1912d7c
Start Coding Student 2 <session2@START-Coding-MBP-09.local>
2016-04-13 17:44:37 -0700

    no message

---
+++ Sign Up.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Sign Up </title>
+    <link href="https://assets.onestore.ms/cdnfiles/onestorerolling-1601-22000/shell/v3/scss/shell.min.css"
+          rel="stylesheet" type="text/css" media="screen"/>
+    <link href="signin.css" rel="stylesheet" type="text/css">
+    <script src="signin.js" type="text/javascript"></script>
+</head>
+<body>
+<?php
+try {
+    $dbh = new PDO('mysql:host=127.0.0.1;dbname=subdb', 'root', 'root');
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+die();
+}
+if(@$_POST['formSubmit'] == "Submit")
+{
+$errorMessage = "";
+if(empty($_POST['username']))
+{
+$errorMessage = "<li>You forgot to enter your username.</li>";
+}
+if(empty($_POST['email']))
+{
+$errorMessage = "<li>You forgot to enter your email.</li>";
+}
+if(empty($_POST['password']))
+{
+$errorMessage = "<li>You forgot to enter your last password.</li>";
+}
+$stmt = $dbh->prepare("INSERT INTO users (username, email, password ) VALUES (?, ?, ?)");
+$result = $stmt->execute(array($_POST['username'], $_POST['email'], $_POST['password']));
+if(!$result){
+print_r($stmt->errorInfo());
+}
+else{
+$msg = 'Thank you for subscribing to Shelved.';
+$from = 'admin@shelved.com';
+mail($_POST['email'], 'Shelved Subscription' , $msg, 'From:' . $from);
+}
+if(!empty($errorMessage))
+{
+echo("<p>There was an error with your form:</p>\n");
+echo("<ul>" . $errorMessage . "</ul>\n");
+}
+}?>
+<div class="site__container">
+    <h1 style="text-align: center; font-size: 70px;">Sign up</h1>
+    <div class="grid__container">
+        <form method="post" class="form form--login">
+            <div class="form__field">
+                <label class="fontawesome-user" for="login__username"><span class="hidden">Username</span></label>
+                <input type="text" name="username" placeholder="Username or E-mail"/>
+            </div>
+            <div class="form__field">
+                <label class="fontawesome-lock" for="login__password"><span class="hidden">Email</span></label>
+                <input type="text" name="email" placeholder="Email"/>
+            </div>
+            <div class="form__field">
+                <label class="fontawesome-lock" for="login__password"><span class="hidden">Password</span></label>
+                <input type="password" name="password" placeholder="Password"/>
+            </div>
+            <div class="form__field">
+                <input type="submit" name="formSubmit" value="Submit" onclick="newPage()"/>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,27 +3,52 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Suscribe</title>
+    <title>Subscribe</title>
     <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <script src="javascript.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
 </head>
+
 <body>
-
-
-
-
-
-
+<div style="z-index: 10" id='cssmenu'>
+    <ul>
+        <li class="active"><a href='index.php'><span>Home</span></a></li>
+        <li style="float: right;"><a href='Sign%20Up.php'><span>Sign Up</span></a></li>
+    </ul>
+</div>
+​
+<div id = bodyText>
     ​
-    <form method="post" action="account.html">
-        <label for="firstname">First name:</label>
-        <input type="text" id="firstname" name="firstname"/><br/>
-        <label for="lastname">Last name:</label>
-        <input type="text" id="lastname" name="lastname"/><br/>
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email"/><br/>
-        <input type="submit" name="Submit" value="Submit"/>
-    </form>
+</div>
+<?php
+if(isset($_POST['submit'])) {
+    $dbh = new PDO('mysql:host=localhost;dbname=Subscription_Service', 'root', 'root');
+    $query = "INSERT INTO Subscribe (first_name, last_name, email)  VALUES (?,?,?)";
 
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $email = $_POST['email'];
+
+    $stmt = $dbh->prepare($query);
+    $result = $stmt->execute(
+        array($first_name, $last_name, $email)
+    );
+}
+?>
+
+<form action="" method="post">
+    <label for="firstname">First name:</label>
+    <input type="text" id="firstname" name="firstname" /><br />
+    <label for="lastname">Last name:</label>
+    <input type="text" id="lastname" name="lastname" /><br />
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" /><br />
+    <input type="submit" name="submit" value="Submit" />
+</form>
+
+<?php
+
+?>
 
 </body>
 </html>
